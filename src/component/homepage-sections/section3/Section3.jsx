c571292e006893dedc5913ac3242444711f82915
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 22:15:45 +0100

    Completed Section3 of the homepage

--- src/component/homepage-sections/section3/Section3.jsx
+++ src/component/homepage-sections/section3/Section3.jsx
@@ -4,7 +4,17 @@ import './section3.css';
 function Section3() {
     return (
         <section className='nft_info'>
-            <h2>This is section 3</h2>
+            <div className="nft_content">
+                <h2>Metabnb NFTs</h2>
+                <p>Discover our NFT gift cards collection. Loyal customers gets
+                    amazing gift cards which are traded as NFTs.
+                    These NFTs gives our cutomer access to loads of our exclusive services.
+                </p>
+                <button className='nft_button'>Learn more</button>
+            </div>
+            <div className="nft_images">
+                <img src="images/nft-group-image.jpg" alt="" />
+            </div>
         </section>
     );
 }
