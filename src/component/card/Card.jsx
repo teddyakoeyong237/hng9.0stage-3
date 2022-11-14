9f0bd13adc5fcf77489bb0ce5493d7bba4d29f3d
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 15:25:39 +0100

    Created and added Card component to Section2

---
+++ src/component/card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.css';
+
+function Card() {
+    return (
+        <div className='card'>
+            A card
+        </div>
+    );
+}
+
+export default Card;
--- src/component/homepage-sections/section2/Section2.jsx
+++ src/component/homepage-sections/section2/Section2.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Card from '../../card/Card';
 import './section2.css';
 
 function Section2() {
     return (
         <section className='hp_places'>
             <h2>Inspiration for your next adventure</h2>
-            <div className="cards">
-                Cards in here
+            <div className="all_cards">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
             </div>
         </section>
     );
