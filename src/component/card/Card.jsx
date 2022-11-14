1b68d5fa34bacde4e2c0aa5c64e5199774307020
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 18:14:17 +0100

    feat: Completed building Card component

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -4,7 +4,26 @@ import './card.css';
 function Card() {
     return (
         <div className='card'>
-            A card
+            <div className="card_image">
+                <img src="images/image1.jpg" alt="Place pic" />
+            </div>
+            <div className="card_content">
+                <div className="content content1">
+                    <p>Desert king</p>
+                    <p><b>1MBT per night</b></p>
+                </div>
+                <div className="content content2">
+                    <p>2345km away</p>
+                    <p>available for 2weeks stay</p>
+                </div>
+            </div>
+            <div className="stars">
+                <img src="images/star.jpg" alt="" />
+                <img src="images/star.jpg" alt="" />
+                <img src="images/star.jpg" alt="" />
+                <img src="images/star.jpg" alt="" />
+                <img src="images/star.jpg" alt="" />
+            </div>
         </div>
     );
 }
