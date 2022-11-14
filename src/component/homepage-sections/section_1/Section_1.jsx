b665f0f20ccea4ddb6de58131776d495902524b3
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 14:30:18 +0100

    Built section1 of homepage

--- src/component/homepage-sections/section_1/Section_1.jsx
+++ src/component/homepage-sections/section_1/Section_1.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import './section_1.css';
 
 function Section_1() {
     return (
-        <section className='search_place'>
-            <div className='sp_content'>
+        <>
+            <section className='search_place'>
+                <div className='sp_content'>
+                    <h2>Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span></h2>
+                    <p>we provide you access to luxury and affordable houses in the metaverse,
+                        get a chance to turn your imagination to reality at your comfort zone</p>
+                    <div className='search_container'>
+                        <input
+                            className='search_input'
+                            type="search" name="" id=""
+                            placeholder='Search for location' />
+                        <button className='search_button'>Search</button>
+                    </div>
+                </div>
+                <div className='sp_images'>
+                    <img src="images/sp-group-image.jpg" alt="Sample of places" />
+                </div>
+            </section>
+            <div className='logo_banner'>
+                <img src="images/mbtoken_logo.jpg" alt="Mbtoken Logo" />
+                <img src="images/metamask_logo.jpg" alt="Metamask Logo" />
+                <img src="images/opensea_logo.jpg" alt="Opensea Logo" />
             </div>
-            <div className='sp_images'>
-                <img src="images/sp-group-image.jpg" alt="Sample of places" />
-            </div>
-        </section>
+        </>
     );
 }
 
