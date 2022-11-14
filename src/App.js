751289ec056c1eb810c5b7e44fcba6541f4a7613
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 07:21:41 +0100

    added Homepage page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './component/navbar/Navbar';
+import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homepage />
       <h2>HNG STAGE 3 TASK</h2>
     </div>
   );
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Section_1 from '../../component/homepage-sections/section_1/Section_1';
+import './homepage.css';
+
+function Homepage() {
+    return (
+        <div className='homepage'>
+            <Section_1 />
+        </div>
+    );
+}
+
+export default Homepage;
