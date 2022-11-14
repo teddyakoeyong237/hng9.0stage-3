29f1b8bae58ffae1c8a812f0fcc4623423587f8c
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 05:27:46 +0100

    Created navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './component/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>HNG STAGE 3 TASK</h1>
+      <Navbar />
+      <h2>HNG STAGE 3 TASK</h2>
     </div>
   );
 }
---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <div className='navbar'> 
+            <header>
+                <h1>Creating Navbar Component</h1>
+                <nav>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
