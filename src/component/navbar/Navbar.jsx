5ec717b276ae73d949c783ca3b75953d4c735feb
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 06:56:27 +0100

    built navbar component

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <div className='navbar'> 
-            <header>
-                <h1>Creating Navbar Component</h1>
+            <div className="container">
+        <header className='navbar'>
+                <img src="images/colored_metabnb.jpg" alt="logo" />
                 <nav>
                     <ul>
-                        <li></li>
+                        <li><Link to="">Home</Link></li>
+                        <li><Link to="">Place to stay</Link></li>
+                        <li><Link to="">NFTs</Link></li>
+                        <li><Link to="">Community</Link></li>
                     </ul>
                 </nav>
+                    <button className='connect_wallet'>Connect wallet</button>
             </header>
-        </div>
+            </div>
     );
 }
