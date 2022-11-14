721ff388a35e545c7fec21b1f8d9a96f9a79a051
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 20:47:30 +0100

    Added all images to cards

--- src/component/homepage-sections/section2/Section2.jsx
+++ src/component/homepage-sections/section2/Section2.jsx
@@ -8,13 +8,13 @@ function Section2() {
             <h2>Inspiration for your next adventure</h2>
             <div className="all_cards">
                 <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card />
+                <Card image_url={"images/image2.jpg"}/>
+                <Card image_url={"images/image3.jpg"}/>
+                <Card image_url={"images/image4.jpg"}/>
+                <Card image_url={"images/image5.jpg"}/>
+                <Card image_url={"images/image6.jpg"}/>
+                <Card image_url={"images/image7.jpg"}/>
+                <Card image_url={"images/image8.jpg"}/>
             </div>
         </section>
     );
