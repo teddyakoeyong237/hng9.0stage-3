511008cff0e11895c932d9e3cdc413a37c962a27
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 07:24:08 +0100

    Created first section of the homepage

---
+++ src/component/homepage-sections/section_1/Section_1.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Section_1() {
+    return (
+        <section className='search_place'>
+            <div className='sp_content'>
+            </div>
+            <div className='sp_images'>
+                <img src="images/sp-group-image.jpg" alt="Sample of places" />
+            </div>
+        </section>
+    );
+}
+
+export default Section_1;
