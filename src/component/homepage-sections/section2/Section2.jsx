e8ec52abb3544db29ccd28f5f68dc56f6eb7f947
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-14 15:05:45 +0100

    feat: Added section2 of homepage(cards section)

---
+++ src/component/homepage-sections/section2/Section2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './section2.css';
+
+function Section2() {
+    return (
+        <section className='hp_places'>
+            <h2>Inspiration for your next adventure</h2>
+            <div className="cards">
+                Cards in here
+            </div>
+        </section>
+    );
+}
+
+export default Section2;
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Section1 from '../../component/homepage-sections/section1/Section1';
+import Section2 from '../../component/homepage-sections/section2/Section2';
 import './homepage.css';
 
 function Homepage() {
     return (
         <div className='homepage'>
             <Section1 />
+            <Section2 />
         </div>
     );
 }
