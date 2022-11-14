013207d047a3c35b54c485c8f009c4986f130b59
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-15 00:12:22 +0100

    feat: Completed footer feature

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -6,40 +6,47 @@ function Footer() {
         <div className='footer'>
             <div className="top_footer">
                 <div className="footer_logo">
-
+                    <div className="footer_logo_img">
+                        <img src="images/footer_logo.jpg" alt="" />
+                    </div>
+                    <div className="socialmedia_icons">
+                        <img src="images/facebook_logo.jpg" alt="" />
+                        <img src="images/instagram_logo.jpg" alt="" />
+                        <img src="images/twitter_logo.jpg" alt="" />
+                    </div>
                 </div>
                 <div className="footer_menu">
                     <div>
-                        <h5></h5>
+                        <h5>Community</h5>
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>NFT</li>
+                            <li>Tokens</li>
+                            <li>Landlords</li>
+                            <li>Discord</li>
                         </ul>
                     </div>
                     <div>
-                        <h5></h5>
+                        <h5>Places</h5>
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>Castle</li>
+                            <li>Farms</li>
+                            <li>Beach</li>
+                            <li>Learn more</li>
                         </ul>
                     </div>
                     <div>
-                        <h5></h5>
+                        <h5>About us</h5>
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>Road map</li>
+                            <li>Creators</li>
+                            <li>Career</li>
+                            <li>Contact us</li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div className="bottom_footer">
-                <p>&copy;</p>
+                &copy; <p> 2022 Metabnb</p>
             </div>
         </div>
     );
