c03d081951b1d7df4ceebc2351ced6e9fc254bc7
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-15 11:04:51 +0100

    Created new page (Place to stay)

---
+++ src/pages/placetostay/PlaceToStay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './placetostay.css';
+
+function PlaceToStay() {
+    return (
+        <div className='placetostay'>
+
+        </div>
+    );
+}
+
+export default PlaceToStay;
