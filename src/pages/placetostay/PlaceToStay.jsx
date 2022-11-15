8ad9ddc84996ec6c08a2e4a7aea5ecca28a93586
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-15 13:49:41 +0100

    Added all images to Placetostay page

--- src/pages/placetostay/PlaceToStay.jsx
+++ src/pages/placetostay/PlaceToStay.jsx
@@ -19,22 +19,22 @@ function PlaceToStay() {
                 </ul>
             </div>
             <div className="all_cards">
+                <Card image_url={"images/image5.jpg"}/>
+                <Card image_url={"images/image6.jpg"}/>
+                <Card image_url={"images/image7.jpg"}/>
+                <Card image_url={"images/image8.jpg"}/>
                 <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
-                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image2.jpg"}/>
+                <Card image_url={"images/image3.jpg"}/>
+                <Card image_url={"images/image4.jpg"}/>
+                <Card image_url={"images/placetostay9.jpg"}/>
+                <Card image_url={"images/placetostay10.jpg"}/>
+                <Card image_url={"images/placetostay11.jpg"}/>
+                <Card image_url={"images/placetostay12.jpg"}/>
+                <Card image_url={"images/placetostay13.jpg"}/>
+                <Card image_url={"images/placetostay14.jpg"}/>
+                <Card image_url={"images/placetostay15.jpg"}/>
+                <Card image_url={"images/placetostay16.jpg"}/>
             </div>
         </div>
     );
