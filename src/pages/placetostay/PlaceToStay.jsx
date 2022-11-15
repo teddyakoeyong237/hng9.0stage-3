7e8a0010c51307706af5cb92ce82be02675ceee9
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-15 13:23:48 +0100

    Added location setting icon and built place to stay page

--- src/pages/placetostay/PlaceToStay.jsx
+++ src/pages/placetostay/PlaceToStay.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import Card from '../../component/card/Card';
 import './placetostay.css';
 
 function PlaceToStay() {
     return (
         <div className='placetostay'>
-
+            <div className="categories">
+                <ul>
+                    <li>Restaurant</li>
+                    <li>Cottage</li>
+                    <li>Castle</li>
+                    <li>fantast city</li>
+                    <li>beach</li>
+                    <li>Carbins</li>
+                    <li>Off-grid</li>
+                    <li>Farm</li>
+                    <li className='category_menu'>Location<span><img src="images/setting.jpg" alt="" /></span></li>
+                </ul>
+            </div>
+            <div className="all_cards">
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+                <Card image_url={"images/image1.jpg"}/>
+            </div>
         </div>
     );
 }
