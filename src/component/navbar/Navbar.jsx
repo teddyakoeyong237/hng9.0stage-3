c482f45d6ab46abc04222105ecddee7e99ea38ba
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-16 09:59:40 +0100

    navbar and modal responsiveness

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
 import './navbar.css';
 import { Link } from 'react-router-dom';
 import Modal from '../modal/Modal';
 
 export default function Navbar() {
+
+    const navRef = useRef();
+
+    const showNavbar  = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
 
     const [openModal, setOpenModal] = useState(false);
 
@@ -17,19 +24,29 @@ export default function Navbar() {
 
         <header className='navbar'>
                 <img src="images/colored_metabnb.jpg" alt="logo" />
-                <nav>
+                <nav ref={navRef}>
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/placetostay">Place to stay</Link></li>
                         <li><Link to="/nfts">NFTs</Link></li>
                         <li><Link to="/community">Community</Link></li>
+                        <li>
+                            <button
+                            onClick={() => {setOpenModal(true)}} 
+                            className='connect_wallet'
+                            >
+                                Connect wallet
+                            </button>
+                        </li>
+                        <li>
+                            <button className='nav-btn nav-close-btn' onClick={showNavbar} >
+                                <FaTimes />
+                            </button>
+                        </li>
                     </ul>
                 </nav>
-                    <button
-                    onClick={() => {setOpenModal(true)}} 
-                    className='connect_wallet'
-                    >
-                        Connect wallet
+                    <button className='nav-btn' onClick={showNavbar} >
+                        <FaBars />
                     </button>
                     <Modal openState={openModal} onClose={() => {setOpenModal(false)}}/>
             </header>
