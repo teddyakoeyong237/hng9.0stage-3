c9933371d153318465ee573aab1e465f3b19acd6
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-16 01:15:03 +0100

    Finished building modal component

--- src/component/modal/Modal.jsx
+++ src/component/modal/Modal.jsx
@@ -9,12 +9,39 @@ function Modal({openState, onClose}) {
 
   return (
     <div onClick={onClose} className="modal_overlay">
+        
+        {/* Creating modal component */}
         <div className='modal'>
+            
+            {/* Creating the header of the modal component */}
             <div className="modal_header">
                 <h3>Connect Wallet</h3>
                 <span>
-                    <img src="images/modalclose.jpg" alt="" />
+                    <img src="images/modalclose.jpg" alt="Close modal" />
                 </span>
+            </div>
+
+            {/* Creating the body of the modal component  */}
+            <div className="modal_body">
+                <p>Choose your preferred wallet:</p>
+
+                {/* Creating the links in the modal  */}
+                <div className="modal_links">
+                    <div className="modal_link_img">
+                        <img src="images/modalgrp1.jpg" alt="" />
+                    </div>
+                    <span className="modal_arrow">
+                        <img src="images/modalarrow.svg" alt="" />
+                    </span>
+                </div>
+                <div className="modal_links">
+                    <div className="modal_link_img">
+                        <img src="images/modalgrp2.jpg" alt="" />
+                    </div>
+                    <span className="modal_arrow">
+                        <img src="images/modalarrow.svg" alt="" />
+                    </span>
+                </div>
             </div>
         </div>
     </div>
